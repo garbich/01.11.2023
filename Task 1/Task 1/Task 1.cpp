00bff1b192973227501786420ed5c68e077ba4e2
garbich <131876643+garbich@users.noreply.github.com>
2023-11-01 20:42:46 +0200

    Add files via upload

---
+++ Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+using namespace std;
+
+class Hero {
+public:
+    void virtual spawnedHero() = 0;
+};
+
+class Enemy {
+public:
+    void virtual spawnedEnemy() = 0;
+};
+
+class Knight : public Hero {
+public:
+    void spawnedHero() override {
+        cout << "knight was spawned" << endl;
+    }
+};
+
+class Archer : public Hero {
+public:
+    void spawnedHero() override {
+        cout << "archer was spawned" << endl;
+    }
+};
+
+class Goblin : public Enemy {
+public:
+    void spawnedEnemy() override {
+        cout << "goblin was spawned" << endl;
+    }
+};
+
+class Witch : public Enemy {
+public:
+    void spawnedEnemy() override {
+        cout << "witch was spawned" << endl;
+    }
+};
+
+class GameFactory {
+public:
+    virtual  Hero* createHero() = 0;
+    virtual  Enemy* createEnemy() = 0;
+};
+
+class spawnedFactory : public GameFactory {
+public:
+    Hero* createHero() override {
+        Hero* knight = new Knight();
+        return knight;
+    }
+
+
+
+    Enemy* createEnemy() override {
+        Enemy* goblin = new Goblin();
+        return goblin;
+    }
+
+
+};
+
+class FantasyGameFactory : public GameFactory {
+public:
+    Hero* createHero() override {
+        Hero* hero = new Knight();
+        return hero;
+    };
+
+    Enemy* createEnemy() override {
+        Enemy* enemy = new Goblin();
+        return enemy;
+    }
+};
+
+
+class SciFiGameFactory : public GameFactory {
+public:
+    Hero* createHero() override {
+        Hero* hero = new Archer();
+        return hero;
+    };
+
+    Enemy* createEnemy() override {
+        Enemy* enemy = new Witch();
+        return enemy;
+    }
+};
+
+class Player {
+public:
+    GameFactory* gameFactory;
+
+    Player(GameFactory* _gameFactory)
+        : gameFactory(_gameFactory) {}
+
+    void SpawnedHero() {
+        Hero* hero = gameFactory->createHero();
+        hero->spawnedHero();
+
+    }
+
+    void spawnedEnemy() {
+        Enemy* enemy = gameFactory->createEnemy();
+        enemy->spawnedEnemy();
+    }
+};
+
+int main()
+{
+    GameFactory* gameFactory = new FantasyGameFactory();
+
+    Player* player = new Player(gameFactory);
+
+    player->SpawnedHero();
+    player->spawnedEnemy();
+
+
+}
---
+++ Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Singlton {
+private:
+	Singlton() {}
+
+
+	vector<string> getEntries;
+public:
+	static Singlton& getInstence() {
+		static Singlton instence;
+		return instence;
+	}
+
+
+
+	vector<string> getLogs() {
+		return this->getEntries;
+	}
+
+	void log(string text) {
+		getEntries.push_back(text);
+	}
+};
+
+int main()
+{
+	Singlton& singlton = Singlton::getInstence();
+
+	singlton.log("Error: failed to read the file");
+	singlton.log("Information: downloaded data from the server");
+
+
+	vector<string> logEntries = singlton.getLogs();
+
+	for (int i = 0; i < logEntries.size(); i++) {
+		cout << logEntries[i] << endl;
+	}
+
+	
+
+
+}
